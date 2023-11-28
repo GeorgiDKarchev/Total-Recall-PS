14fc87d0515ba2817c72e0a32cfc690905b93f95
[Georgi Karchev] <georgikarcev@gmail.com>
2023-11-27 22:41:51 -0500

    Functions task

--- Section1/src/index.js
+++ Section1/src/index.js
@@ -57,7 +57,128 @@ let it = "cow";
         console.log(`Hey! You're not a cow.`)
     }
 
+//###E. Driver's Ed
+console.log(`--------------###E. Driver's Ed------------`);
+ 
+let agePerson = 16;
+ if(agePerson>=16){
+    console.log("Here are the keys!")
+ } else {
+    console.log("Sorry, you're too young.")
+ }
+
+ //## II. Loops
+ console.log(`--------------## II. Loops-----------`)
+//A.the basics
+console.log(`--------form 0 to 10-------`)
+for (let i=0; i<=10; i++){
+    console.log(i);
+};
+ console.log(`-----from 10 to 400-----`)
+for (let x=10; x<=400; x++){
+    console.log(x);
+}
+
+console.log(`-------every third # starting with 12  up to to 4000-----`)
+
+for (let g=12; g<=4000; g++){
+    if (g % 3 == 0){
+        console.log(g);
+    }
+    
+}
+
+//### B. Get even
+
+console.log(`-----------### B. Get even--------`)
+
+for (let e=1; e<=100; e++){
+    console.log(e);
+}
+
+let data = {
+
+       a: 10,
+    
+      b: 20,
+    
+      c: 30,
+    
+    };
+    
+    console.log(Object.keys(data).length);
+
+// 5 Function.---------------------------
+//a----------
+
+let name = 'Slimer'
+
+function printGreeting(name){
+    console.log(`Hello there,${name}!`)
+}
+ printGreeting('Jason')
+ printGreeting('Alex')
+ printGreeting(name)
+
+//B ------------------
+
+function printCool(name){
+     //console.log(`${name} is cool.`)
+     return`${name} is cool.`
+
+}
+//printCool (name)
+//printCool ('Alex')
+//printCool ('Captain Reynolds')
+console.log(printCool("Captain Reynolds"));
 
 
+// C ------- Calculate Cube---------------------------------------
+
+function calculateCube(num){
+    return num ** 3
+}
+console.log(calculateCube(5))
+
+// D --------isVowel--------------------------------------------
 
 
+function isVowelThat(char){
+    const lowerCase = char.toLowerCase()
+    return lowerCase === 'a' || char==='e'|| char==='i' ||char=== 'o'||char ==='u'
+
+    
+}
+ console.log(isVowelThat('e'))
+
+// E  -----------getTwo Lengths--------------------------------------
+
+function  getTwoLengths (string1, string2){
+    return [string1.length, string2.length]
+}
+ console.log(getTwoLengths('Hank', 'Hippopopalos'))
+ console.log(getTwoLengths('Hello World','Hippopopalos'))
+
+//F getMultipleLengths----------------------------------------------
+
+function getMultipleLengths (arr) {
+   const newArr = []
+ /*   for (let i=0; i < arr.length; i++){
+    newArr.push(arr[i].length)
+   } */
+  /*  for (const string of arr){
+    newArr.push(string.length)
+   } */
+   arr.forEach(string =>{
+     newArr.push(string.length)
+   })
+   console.log(newArr)
+}
+
+getMultipleLengths(["hello", "what", "is", "up", "dude"])
+
+//G-----maxOfThree
+
+ 
+
+ 
